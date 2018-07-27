1e47f0aa02c02da068adf5ce299eebed9a631efd
Jennifer Vargas <jennvargas1990@outlook.com>
2018-07-27 11:28:11 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+(document).ready(function($) { 
+    $(window).scroll(function() { 
+        var scrollTop = $(window).scrollTop(); 
+        var elementTop = $('.main-headline').offset().top;
+
+        if(scrollTop >= elementTop) {
+            $('.main-headline img').addClass('scrolled');
+        }
+    });
+});
+
+(document).ready(function($) { 
+    $(window).scroll(function() { 
+        var scrollTop = $(window).scrollTop(); 
+        var elementTop = $('.section1').offset().top;
+    
+       if(scrollTop >= elementTop) {
+            $('.section1 img').addClass('scrolled');
+        }
+    });
+});
+
+(document).ready(function($) { 
+    $(window).scroll(function() { 
+        var scrollTop = $(window).scrollTop(); 
+        var elementTop = $('.section2').offset().top;
+    
+       if(scrollTop >= elementTop) {
+            $('.section2 img').addClass('scrolled');
+        }
+    });
+});
+
+(document).ready(function($) { 
+    $(window).scroll(function() { 
+        var scrollTop = $(window).scrollTop(); 
+        var elementTop = $('.section3').offset().top;
+    
+       if(scrollTop >= elementTop) {
+            $('.section3 img').addClass('scrolled');
+        }
+    });
+});
